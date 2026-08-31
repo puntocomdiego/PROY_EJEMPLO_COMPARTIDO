42e5e7f2b037363d648dd312fd0aa0c791a909d6
diego aldorasi <dialdo@hotmail.com>
2026-08-31 15:23:13 -0300

    MODIFICA COD BOTONES FORM1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,6 +23,12 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
         SuspendLayout()
         ' 
         ' Button1
@@ -31,14 +37,74 @@ Partial Class Form1
         Button1.Name = "Button1"
         Button1.Size = New Size(224, 99)
         Button1.TabIndex = 0
-        Button1.Text = "Button1"
+        Button1.Text = "HECTOR"
         Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(346, 43)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(224, 99)
+        Button2.TabIndex = 1
+        Button2.Text = "KEVIN"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(94, 148)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(224, 99)
+        Button3.TabIndex = 2
+        Button3.Text = "ALEXIS"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(346, 148)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(224, 99)
+        Button4.TabIndex = 3
+        Button4.Text = "NAZA"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(94, 253)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(224, 99)
+        Button5.TabIndex = 4
+        Button5.Text = "TABA"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(346, 253)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(224, 99)
+        Button6.TabIndex = 5
+        Button6.Text = "DANIEL"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(94, 374)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(224, 99)
+        Button7.TabIndex = 6
+        Button7.Text = "NACHO"
+        Button7.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(800, 646)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
@@ -46,5 +112,11 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,44 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim F As New FRM_HESCTOR
+        F.Show()
 
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim F As New FRM_KEVIN
+        F.Show()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim F As New FRM_ALEXIS
+        F.Show()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim F As New FRM_NAZA
+
+        F.Show()
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim F As New FRM_TABA
+        F.Show()
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim F As New FRM_DANIEL
+        F.Show()
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim F As New FRM_NACHO
+        F.Show()
+
+    End Sub
 End Class
