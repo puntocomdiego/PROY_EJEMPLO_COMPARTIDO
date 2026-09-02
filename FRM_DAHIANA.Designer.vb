821dba3fa9ed2ca142eab2c5e90f000228090c02
Daiana <daianac99@hotmail.com>
2026-09-01 21:52:30 -0300

    Actualizado 21:49

--- FRM_DAHIANA.Designer.vb
+++ FRM_DAHIANA.Designer.vb
@@ -22,9 +22,52 @@ Partial Class FRM_DAHIANA
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FRM_DAHIANA"
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(316, 76)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(150, 67)
+        Button1.TabIndex = 0
+        Button1.Text = "CLICK!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(474, 216)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(150, 67)
+        Button2.TabIndex = 1
+        Button2.Text = "CLICK!"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(121, 216)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(150, 67)
+        Button3.TabIndex = 2
+        Button3.Text = "CLICK!"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' FRM_DAHIANA
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "FRM_DAHIANA"
+        Text = "FRM_DAHIANA"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- FRM_DAHIANA.vb
+++ FRM_DAHIANA.vb
@@ -1,3 +1,20 @@
 ﻿Public Class FRM_DAHIANA
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        Button2.BackColor = Color.Blue
+        Button2.ForeColor = Color.White
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Button3.BackColor = Color.Red
+        Button2.ForeColor = Color.White
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Button1.BackColor = Color.Yellow
+
+    End Sub
 End Class
