def3a10f31e38f6c520e95076400a3e487ade9a3
Jorge Miraballes <jorgemiraballes@gmail.com>
2026-09-01 21:55:10 -0300

    hola

--- FRM_JORGE.Designer.vb
+++ FRM_JORGE.Designer.vb
@@ -22,9 +22,28 @@ Partial Class FRM_JORGE
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FRM_JORGE"
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(128, 90)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(221, 106)
+        Button1.TabIndex = 0
+        Button1.Text = "Presiona"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' FRM_JORGE
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Name = "FRM_JORGE"
+        Text = "FRM_JORGE"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
--- FRM_JORGE.vb
+++ FRM_JORGE.vb
@@ -1,3 +1,5 @@
 ﻿Public Class FRM_JORGE
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Y Explota")
+    End Sub
 End Class
