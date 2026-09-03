2b1ad524951b2f2c9314634bb7b12f844dc19b04
Tabbbb <tabareapecet@gmail.com>
2026-09-03 20:49:23 -0300

    ok

--- FRM_TABA.vb
+++ FRM_TABA.vb
@@ -123,8 +123,4 @@
             Exit Sub
         End If
     End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
 End Class
