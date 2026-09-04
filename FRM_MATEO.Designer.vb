033f5a1234f40d281d2070a0061ccfb18949c286
5upr3m4 <dossantosteo0@gmail.com>
2026-09-03 21:38:39 -0300

    actualización 21:38

--- FRM_MATEO.Designer.vb
+++ FRM_MATEO.Designer.vb
@@ -17,6 +17,7 @@ Partial Class FRM_MATEO
 
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FRM_MATEO))
         lblConsigna = New Label()
         Button1 = New Button()
         txtNum1 = New TextBox()
@@ -24,33 +25,33 @@ Partial Class FRM_MATEO
         lblNum1 = New Label()
         lblNum2 = New Label()
         lblResultado = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
         SuspendLayout()
         ' 
         ' lblConsigna
         ' 
         lblConsigna.BackColor = Color.LightYellow
         lblConsigna.BorderStyle = BorderStyle.FixedSingle
-        lblConsigna.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular)
+        lblConsigna.Font = New Font("Segoe UI", 9.75F)
         lblConsigna.Location = New Point(12, 12)
         lblConsigna.Name = "lblConsigna"
         lblConsigna.Padding = New Padding(8)
         lblConsigna.Size = New Size(770, 120)
         lblConsigna.TabIndex = 1
-        lblConsigna.Text = "TRABAJO PRACTICO - Sumar dos numeros" & vbCrLf & vbCrLf &
-            "Hay dos cuadros de texto (txtNum1 y txtNum2). Programa el boton para que sume los dos" & vbCrLf &
-            "numeros ingresados y muestre el resultado en la etiqueta ""lblResultado""." & vbCrLf &
-            "Validar con IsNumeric que ambos sean numeros; si no, mostrar un aviso." & vbCrLf &
-            "Extra (opcional): agrega botones para restar, multiplicar y dividir."
+        lblConsigna.Text = resources.GetString("lblConsigna.Text")
         ' 
-        ' lblNum1
+        ' Button1
         ' 
-        lblNum1.AutoSize = True
-        lblNum1.Font = New Font("Segoe UI", 12F)
-        lblNum1.Location = New Point(150, 160)
-        lblNum1.Name = "lblNum1"
-        lblNum1.Size = New Size(80, 21)
-        lblNum1.TabIndex = 2
-        lblNum1.Text = "Numero 1:"
+        Button1.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button1.Font = New Font("Segoe UI", 12F)
+        Button1.Location = New Point(148, 250)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(86, 50)
+        Button1.TabIndex = 0
+        Button1.Text = "Sumar +"
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' txtNum1
         ' 
@@ -60,16 +61,6 @@ Partial Class FRM_MATEO
         txtNum1.Size = New Size(150, 29)
         txtNum1.TabIndex = 3
         ' 
-        ' lblNum2
-        ' 
-        lblNum2.AutoSize = True
-        lblNum2.Font = New Font("Segoe UI", 12F)
-        lblNum2.Location = New Point(150, 205)
-        lblNum2.Name = "lblNum2"
-        lblNum2.Size = New Size(80, 21)
-        lblNum2.TabIndex = 4
-        lblNum2.Text = "Numero 2:"
-        ' 
         ' txtNum2
         ' 
         txtNum2.Font = New Font("Segoe UI", 12F)
@@ -78,15 +69,25 @@ Partial Class FRM_MATEO
         txtNum2.Size = New Size(150, 29)
         txtNum2.TabIndex = 5
         ' 
-        ' Button1
+        ' lblNum1
         ' 
-        Button1.Font = New Font("Segoe UI", 12F)
-        Button1.Location = New Point(260, 250)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(150, 50)
-        Button1.TabIndex = 0
-        Button1.Text = "SUMAR"
-        Button1.UseVisualStyleBackColor = True
+        lblNum1.AutoSize = True
+        lblNum1.Font = New Font("Segoe UI", 12F)
+        lblNum1.Location = New Point(150, 160)
+        lblNum1.Name = "lblNum1"
+        lblNum1.Size = New Size(84, 21)
+        lblNum1.TabIndex = 2
+        lblNum1.Text = "Numero 1:"
+        ' 
+        ' lblNum2
+        ' 
+        lblNum2.AutoSize = True
+        lblNum2.Font = New Font("Segoe UI", 12F)
+        lblNum2.Location = New Point(150, 205)
+        lblNum2.Name = "lblNum2"
+        lblNum2.Size = New Size(84, 21)
+        lblNum2.TabIndex = 4
+        lblNum2.Text = "Numero 2:"
         ' 
         ' lblResultado
         ' 
@@ -96,11 +97,47 @@ Partial Class FRM_MATEO
         lblResultado.Size = New Size(500, 40)
         lblResultado.TabIndex = 6
         ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button2.Font = New Font("Segoe UI", 12F)
+        Button2.Location = New Point(260, 250)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(86, 50)
+        Button2.TabIndex = 7
+        Button2.Text = "Restar -"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(192), CByte(192), CByte(255))
+        Button3.Font = New Font("Segoe UI", 12F)
+        Button3.Location = New Point(372, 250)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(105, 50)
+        Button3.TabIndex = 8
+        Button3.Text = "Multiplicar x"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        Button4.Font = New Font("Segoe UI", 12F)
+        Button4.Location = New Point(498, 250)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(86, 50)
+        Button4.TabIndex = 9
+        Button4.Text = "Dividir %"
+        Button4.UseVisualStyleBackColor = False
+        ' 
         ' FRM_MATEO
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
         Controls.Add(lblResultado)
         Controls.Add(txtNum2)
         Controls.Add(lblNum2)
@@ -121,4 +158,7 @@ Partial Class FRM_MATEO
     Friend WithEvents lblNum1 As Label
     Friend WithEvents lblNum2 As Label
     Friend WithEvents lblResultado As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 End Class
--- FRM_MATEO.vb
+++ FRM_MATEO.vb
@@ -7,6 +7,67 @@
     ' ============================================================
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("hola")   ' <-- reemplaza por tu logica de suma
+
+        If IsNumeric(txtNum1.Text) And IsNumeric(txtNum2.Text) Then
+            Dim num1 As Double = txtNum1.Text
+            Dim num2 As Double = txtNum2.Text
+            Dim resultado = num1 + num2
+            lblResultado.Text = "Resultado: " & resultado.ToString
+        Else
+            MessageBox.Show("Por favor, ingrese números válidos en ambos campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If IsNumeric(txtNum1.Text) And IsNumeric(txtNum2.Text) Then
+            Dim num1 As Double = txtNum1.Text
+            Dim num2 As Double = txtNum2.Text
+            Dim resultado = num1 - num2
+            lblResultado.Text = "Resultado: " & resultado.ToString
+        Else
+            MessageBox.Show("Por favor, ingrese números válidos en ambos campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If IsNumeric(txtNum1.Text) And IsNumeric(txtNum2.Text) Then
+            Dim num1 As Double = txtNum1.Text
+            Dim num2 As Double = txtNum2.Text
+            Dim resultado = num1 * num2
+            lblResultado.Text = "Resultado: " & resultado.ToString
+        Else
+            MessageBox.Show("Por favor, ingrese números válidos en ambos campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        If IsNumeric(txtNum1.Text) And IsNumeric(txtNum2.Text) Then
+            Dim num1 As Double = txtNum1.Text
+            Dim num2 As Double = txtNum2.Text
+            Dim resultado = num1 / num2
+            lblResultado.Text = "Resultado: " & resultado.ToString
+        Else
+            MessageBox.Show("Por favor, ingrese números válidos en ambos campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub txtNum1_GotFocus(sender As Object, e As EventArgs) Handles txtNum1.GotFocus
+        txtNum1.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub txtNum1_LostFocus(sender As Object, e As EventArgs) Handles txtNum1.LostFocus
+        txtNum1.BackColor = Color.White
+    End Sub
+
+    Private Sub txtNum2_GotFocus(sender As Object, e As EventArgs) Handles txtNum2.GotFocus
+        txtNum2.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub txtNum2_LostFocus(sender As Object, e As EventArgs) Handles txtNum2.LostFocus
+        txtNum2.BackColor = Color.White
     End Sub
 End Class
