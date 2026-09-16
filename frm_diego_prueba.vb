1807588a383b890f6b72344bfa0b056368d8781b
diego aldorasi <dialdo@hotmail.com>
2026-09-16 18:00:01 -0300

    OK

--- frm_diego_prueba.vb
+++ frm_diego_prueba.vb
@@ -4,7 +4,7 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("hola mundo")
+        MsgBox("hola mundo 2026")
 
     End Sub
 End Class
